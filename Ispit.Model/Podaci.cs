a68cbb1cc0dce2233bed7ab7347c993660e43384
TeaZZL <tea.zlopasa@gmail.com>
2023-07-09 22:23:49 +0200

    Add project files.

---
+++ Ispit.Model/Podaci.cs
@@ -0,0 +1,45 @@
+﻿using Ispit.Model.Klase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ispit.Model
+{
+    public class Podaci
+    {
+        List<Banka> ListaBanki = new List<Banka>() {
+            new Banka() {Simbol="PBZ", Naziv=" Privredna banka Zagreb" } ,
+            new Banka() {Simbol="ZABA", Naziv=" Zagrebačka banka"  },
+            new Banka() {Simbol="OTP", Naziv=" OTP banka" },
+            new Banka() {Simbol="HPB", Naziv=" Hrvatska poštanska banka" }
+        };
+
+        List<Klijent> ListaKlijenata = new List<Klijent>()
+        {
+            new Klijent() {ImePrezime="Ivo Ivić", Stanje=1033450.30, Banka="PBZ"},
+            new Klijent() {ImePrezime="Ana Anić", Stanje=5450, Banka="OTP"},
+            new Klijent() {ImePrezime="Darko Matić", Stanje=800, Banka="ZABA"},
+            new Klijent() {ImePrezime="Jure Jurić", Stanje=34342.1, Banka="PBZ"},
+            new Klijent() {ImePrezime="Pero Petrić", Stanje=5258.30, Banka="PBZ"},
+            new Klijent() {ImePrezime="Vana Ljubić", Stanje=2341117.99, Banka="ZABA"},
+            new Klijent() {ImePrezime="Magda Bogdan", Stanje=1051099.47, Banka="OTP"},
+            new Klijent() {ImePrezime="Matea Petric", Stanje=38814620, Banka="HPB"},
+            new Klijent() {ImePrezime="Nikolina Jozić", Stanje=8523.17, Banka="HPB"},
+            new Klijent() {ImePrezime="Bruno Begić", Stanje=754.83, Banka="PBZ"},
+            new Klijent() {ImePrezime="Slavko Matoš", Stanje=45617.22, Banka="ZABA"},
+            new Klijent() {ImePrezime="Ivan Borić", Stanje=1549873.6, Banka="OTP"}
+        };
+
+        public List<Klijent> DajListuKlijenata
+        {
+            get { return ListaKlijenata; }
+        }
+
+        public List<Banka> DajListuBanki
+        {
+            get { return ListaBanki; }
+        }
+    }
+}
---
+++ Ispit_LINQ/Program.cs
@@ -0,0 +1,58 @@
+﻿using Ispit.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ispit.Model.Klase;
+
+namespace Ispit_LINQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Podaci po = new Podaci();
+
+            var GrupirajPremaBanci = po.DajListuKlijenata.Where(
+                k => k.Stanje >= 1000000.00).GroupBy(
+                p => p.Banka,
+                p => p.ImePrezime,
+                (banka, milijunas) => new GrupiraniMilijunasi()
+                {
+                    Banka = banka,
+                    Mlijunasi = milijunas
+                }
+                ).ToList();
+
+
+            foreach (var rez in GrupirajPremaBanci)
+            {
+                Console.Write("Banka: " + rez.Banka + ": ");
+                foreach (string s in rez.Mlijunasi)
+                { Console.Write(s + "; "); }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+
+            var IzvjestajMilijunasa = po.DajListuKlijenata.Where(p => p.Stanje >= 1000000.00);
+
+            var qs_inner_join = from p in IzvjestajMilijunasa
+                                join t in po.DajListuBanki
+                                on p.Banka equals t.Simbol
+                                where p.Stanje >= 1000000.00
+                                orderby p.Banka
+                                select new
+                                {
+                                    ImeprezimeMilijunasa = p.ImePrezime,
+                                    Banka = t.Naziv
+                                };
+            foreach (var rez in qs_inner_join)
+            {
+                Console.WriteLine(rez.ImeprezimeMilijunasa + " je u " + rez.Banka);
+            }
+
+        }
+    }
+}
